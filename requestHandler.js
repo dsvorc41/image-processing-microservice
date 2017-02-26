839528ad356c9e0edcd0e113d74c2ec2ec49bf91
dsvorc41 <danilosvorcan@live.com>
2017-02-25 18:27:26 -0800

    Add default route

--- requestHandler.js
+++ requestHandler.js
@@ -1,11 +1,11 @@
-
+const sendResponse = require('./utils/sendResponse')
 const setImageHandler = require('./imageHandler/setImage');
 const compareImageHandler = require('./imageHandler/compareImage');
 
 module.exports = {
   landing: (req, res) => { 
     console.log(`Serving ${req.method} request for ${req.url} (inside requestHandler.landing)`);
-    sendResponse(res, 200, headers, 'Welcome the image service for Crustaceans thesis project!');
+    sendResponse(res, 200, '', 'Welcome the image service for Crustaceans thesis project!');
   },
 
   setImage: (req, res) => {
